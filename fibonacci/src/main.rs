6df4740a8368e03a5fd6d79d07a1f842d3a8d64c
rshuva1ov <shuvalov.rem@mail.ru>
2025-12-23 11:47:08 +0300

    fibbonacci parsed array

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -13,6 +13,14 @@ fn main() {
 
     let result = fibonacci(n);
     println!("{}-е число Фибоначчи: {}", n, result);
+
+    let sequence = fibonacci_sequence(n);
+    let formatted = sequence
+        .iter()
+        .map(u64::to_string)
+        .collect::<Vec<_>>()
+        .join(", ");
+    println!("Все значения до n: {}", formatted);
 }
 
 fn fibonacci(n: u64) -> u64 {
@@ -33,4 +41,20 @@ fn fibonacci(n: u64) -> u64 {
     }
 
     curr
+}
+
+fn fibonacci_sequence(n: u64) -> Vec<u64> {
+    if n == 0 {
+        return vec![0];
+    }
+    let mut sequence = Vec::with_capacity((n + 1) as usize);
+    sequence.push(0);
+    sequence.push(1);
+
+    for _ in 2..=n {
+        let next = sequence[sequence.len() - 1] + sequence[sequence.len() - 2];
+        sequence.push(next);
+    }
+
+    sequence
 }
